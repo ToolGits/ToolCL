68004350018761f8f69b0ca90a7ae4f4f5169ae1
GitBob <enzobobdevvideos04@gmail.com>
2026-06-16 17:12:37 -0300

    update the logger.h

--- include/toolcl/logger.h
+++ include/toolcl/logger.h
@@ -1,6 +1,19 @@
 #ifndef TOOLCL_LOGGER_H
 #define TOOLCL_LOGGER_H
 
+typedef enum
+{
+    TOOLCL_DEBUG,
+    TOOLCL_INFO,
+    TOOLCL_WARN,
+    TOOLCL_ERROR
+} ToolCL_LogLevel;
+
 void toolcl_log(const char *message);
+
+void toolcl_log_debug(const char *message);
+void toolcl_log_info(const char *message);
+void toolcl_log_warn(const char *message);
+void toolcl_log_error(const char *message);
 
 #endif
