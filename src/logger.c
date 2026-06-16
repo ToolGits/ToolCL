9af949fd4641ecb0c22925098866b37e6ee60c45
GitBob <enzobobdevvideos04@gmail.com>
2026-06-16 17:13:50 -0300

    Update The logger.c

--- src/logger.c
+++ src/logger.c
@@ -5,3 +5,23 @@ void toolcl_log(const char *message)
 {
     printf("[ToolCL] %s\n", message);
 }
+
+void toolcl_log_debug(const char *message)
+{
+    printf("[DEBUG] %s\n", message);
+}
+
+void toolcl_log_info(const char *message)
+{
+    printf("[INFO ] %s\n", message);
+}
+
+void toolcl_log_warn(const char *message)
+{
+    printf("[WARN ] %s\n", message);
+}
+
+void toolcl_log_error(const char *message)
+{
+    printf("[ERROR] %s\n", message);
+}
