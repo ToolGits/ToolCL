157faf03f0d753a9a6c31f69ea980596e5d32818
GitBob <enzobobdevvideos04@gmail.com>
2026-06-16 08:00:58 -0300

    Create math.c

---
+++ src/math.c
@@ -0,0 +1,24 @@
+#include "toolcl/math.h"
+
+int toolcl_add(int a, int b)
+{
+    return a + b;
+}
+
+int toolcl_sub(int a, int b)
+{
+    return a - b;
+}
+
+int toolcl_mul(int a, int b)
+{
+    return a * b;
+}
+
+int toolcl_div(int a, int b)
+{
+    if (b == 0)
+        return 0;
+
+    return a / b;
+}
